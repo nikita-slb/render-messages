04fcd58da12a3d65cef47aec8ee278bc4d83ecb7
Dmitrii Buza <skyvin7@gmail.com>
2019-04-05 18:02:46 +0300

    component base stuff

--- src/Message.jsx
+++ src/Message.jsx
@@ -2,11 +2,24 @@ import React, {Component} from 'react';
 
 class Message extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            formatType: props.formatType,
+            messageData: JSON.parse(props.message)
+        }
+    }
+
+    renderMessage() {
+        return <MessageNode nodeData={this.state.messageData} />
+    }
+
     render() {
 
         let {message} = this.props;
 
-        console.log(JSON.parse(message.mobile_message));
+        console.log(JSON.parse(message));
 
         return (
             <div className="Mess">
@@ -15,10 +28,100 @@ class Message extends Component {
                         Удалить
                     </button>
                 </div>
-                <div className="MessBody"
-                     dangerouslySetInnerHTML={{__html: message.message}}/>
+
+                <div className="MessBody" >
+                    { this.renderMessage() }
+                </div>
             </div>
         )
+    }
+}
+
+class MessageNode extends Component {
+
+    constructor(props) {
+        super(props);
+
+        const { type, params, children } = props.nodeData;
+
+        this.state = {
+            type,
+            params,
+            children
+        }
+    }
+
+    renderNodeByType({type, params, childNodes}) {
+        const _renderByType_root = () => {
+            return childNodes;
+        };
+
+        const _renderByType_paragraph = () => {
+            return (
+                <p>
+                    { childNodes }
+                </p>
+            )
+        };
+
+        const _renderByType_text = () => {
+
+            switch(params.style) {
+                case 'bold':
+                    return (
+                        <b>
+                            { params.content }
+                            { childNodes }
+                        </b>
+                    );
+                default:
+                    return (
+                        <>
+                           { params.content }
+                           { childNodes }
+                        </>
+                    )
+
+            }
+
+        };
+
+        const _renderByType_link = () => {
+            return (
+                <a href={params.url}>{params.content} </a>
+            )
+        };
+
+        switch (type) {
+            case 'paragraph':
+                return _renderByType_paragraph();
+            case 'root':
+                return _renderByType_root();
+            case 'text':
+                return _renderByType_text();
+            case 'link':
+                return _renderByType_link();
+        }
+    }
+
+    renderNode({ type, params, children }) {
+        const _getNodeChildren = (children) => {
+            return children ? children.map( (childNode, key) => <MessageNode key={key} nodeData={childNode} />) : null;
+        };
+
+        const childNodes = _getNodeChildren(children);
+        return (
+            this.renderNodeByType({ type, params, childNodes })
+        );
+    }
+
+    render() {
+       return (
+           <>
+               { this.renderNode( this.state ) }
+           </>
+
+       )
     }
 }
 
